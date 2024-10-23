df0459fa9b58a2b25acdc929ee19c510c2358d9c
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-10-23 20:35:57 +0700

    feature-renes & function console text

--- testing.js
+++ testing.js
@@ -1,1 +1,11 @@
 const test = "testing dimulai dari sini";
+
+const newFeature = "renes";
+
+function consoleText(text) {
+  for (let i = 0; i < text.length; i++) {
+    console.log(text[i]);
+  }
+}
+
+consoleText(newFeature);
