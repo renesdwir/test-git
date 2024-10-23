2aceb1ffe3be8b77da2a7b233056501bb7c32ca2
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-10-23 21:02:12 +0700

    Revert "Merge pull request #7 from renesdwir/feature-budi"
    This reverts commit b1036caf67a15a749116fe097340f9368d131f59, reversing
    changes made to 20892f3bf57872e02c92533f5625c6f57429f161.

--- testing.js
+++ testing.js
@@ -1,18 +1,1 @@
 const test = "testing dimulai dari sini";
-
-const newFeature = "renes";
-
-function consoleText(text) {
-  for (let i = 0; i < text.length; i++) {
-    console.log(text[i]);
-  }
-}
-
-consoleText(newFeature);
-
-const myName = "budi";
-
-console.log(myName.length);
-
-// release version 0.0.2 dengan membawa feature budi & feature renes
-// feature budi membuat bug harus di revert
